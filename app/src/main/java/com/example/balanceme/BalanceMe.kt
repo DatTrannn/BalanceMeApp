1bed633a4bfcc2b7f067c3809fa8c077d6cd8c4d
DatTrannn <Dat06092001@gmail.com>
2022-08-08 09:41:13 +0800

    Change Color Theme and Init UI

---
+++ app/src/main/java/com/example/balanceme/BalanceMe.kt
@@ -0,0 +1,11 @@
+package com.example.balanceme
+
+import androidx.compose.runtime.Composable
+import com.example.balanceme.ui.theme.BalanceMeTheme
+
+@Composable
+fun BalanceMe() {
+    BalanceMeTheme {
+        
+    }
+}
--- app/src/main/java/com/example/balanceme/ui/theme/Theme.kt
+++ app/src/main/java/com/example/balanceme/ui/theme/Theme.kt
@@ -5,11 +5,12 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import com.example.balanceme.R
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = RichBlack_Light,
+    primaryVariant = RichBlack_Dark,
+    secondary = Gainsboro
 )
 
 private val LightColorPalette = lightColors(
